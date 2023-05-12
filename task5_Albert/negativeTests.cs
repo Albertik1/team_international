74c9dab5bf9300d6a5e9262a9119b1f637a0f43b
unknown <emty0329@gmail.com>
2023-05-12 18:34:05 +0200

    Optimized

--- task5_Albert/negativeTests.cs
+++ task5_Albert/negativeTests.cs
@@ -40,24 +40,54 @@ public class GameProcessorTests
         // Attempt to open another cell after the game is over, which should throw an exception
         Assert.Throws<GameOverException>(() => _gameProcessor.Open(0, 1));
     }
+[Test]
+public void GetCurrentField_GameOver_NotAllCellsRevealed()
+{
+    // Initialize the GameProcessor with a minefield that has a mine in the top-left corner
+    bool[,] mineField = new bool[3, 3] { { true, false, false }, { false, false, false }, { false, false, false } };
+    _gameProcessor = new GameProcessor(mineField);
 
-    [Test]
-    public void GetCurrentField_GameOver_ReturnsAllCells()
+    // Open the top-left cell, which should trigger a game over
+    _gameProcessor.Open(0, 0);
+
+    // Get the current field after the game is over
+    PointState[,] currentField = _gameProcessor.GetCurrentField();
+
+    // Assert that not all cells are revealed
+    bool allCellsRevealed = true;
+    for (int row = 0; row < currentField.GetLength(0); row++)
     {
-        // Initialize the GameProcessor with a minefield that has a mine in the top-left corner
-        bool[,] mineField = new bool[3, 3] { { true, false, false }, { false, false, false }, { false, false, false } };
-        _gameProcessor = new GameProcessor(mineField);
-
-        // Open the top-left cell, which should trigger a game over
-        _gameProcessor.Open(0, 0);
-
-        // GetCurrentField should return a two-dimensional array with all cells revealed
-        PointState[,] expectedField = new PointState[3, 3]
+        for (int col = 0; col < currentField.GetLength(1); col++)
         {
-            { PointState.Mine, PointState.One, PointState.Zero },
-            { PointState.One, PointState.One, PointState.Zero },
-            { PointState.Zero, PointState.Zero, PointState.Zero }
-        };
-        Assert.AreEqual(expectedField, _gameProcessor.GetCurrentField());
+            if (currentField[row, col] == PointState.Closed)
+            {
+                allCellsRevealed = false;
+                break;
+            }
+        }
     }
+
+    Assert.IsFalse(allCellsRevealed);
 }
+
+
+//     [Test]
+//     public void GetCurrentField_GameOver_ReturnsAllCells()
+//     {
+//         // Initialize the GameProcessor with a minefield that has a mine in the top-left corner
+//         bool[,] mineField = new bool[3, 3] { { true, false, false }, { false, false, false }, { false, false, false } };
+//         _gameProcessor = new GameProcessor(mineField);
+
+//         // Open the top-left cell, which should trigger a game over
+//         _gameProcessor.Open(0, 0);
+
+//         // GetCurrentField should return a two-dimensional array with all cells revealed
+//         PointState[,] expectedField = new PointState[3, 3]
+//         {
+//             { PointState.Mine, PointState.One, PointState.Zero },
+//             { PointState.One, PointState.One, PointState.Zero },
+//             { PointState.Zero, PointState.Zero, PointState.Zero }
+//         };
+//         Assert.AreEqual(expectedField, _gameProcessor.GetCurrentField());
+//     }
+// }
--- task5_Albert/positiveTests.cs
+++ task5_Albert/positiveTests.cs
@@ -83,5 +83,92 @@ namespace Minesweeper.Tests
                                                                  { PointState.Closed, PointState.Closed, PointState.Closed } };
             Assert.AreEqual(expectedField, newState.Field);
         }
+
+        [Test]
+        public void OpenMethod_ReturnsGameStateWithCorrectPointStates_AfterMultipleOpeningCells()
+        {
+            // Arrange
+            bool[,] field = new bool[4, 4] { { false, true, false, false }, { true, false, true, false }, { false, true, false, true }, { false, false, true, false } };
+            GameProcessor game = new GameProcessor(field);
+
+            // Act
+            GameState newState = game.Open(0, 0);
+            newState = game.Open(1, 0);
+            newState = game.Open(2, 1);
+            newState = game.Open(3, 2);
+
+            // Assert
+            PointState[,] expectedField = new PointState[4, 4] { { PointState.Opened,
+        [Test]
+        public void OpenMethod_ReturnsGameStateWithCorrectPointStates_AfterMultipleOpeningCells()
+        {
+            // Arrange
+            bool[,] field = new bool[4, 4] { { false, true, false, false }, { true, false, true, false }, { false, true, false, true }, { false, false, true, false } };
+            GameProcessor game = new GameProcessor(field);
+
+            // Act
+            GameState newState = game.Open(0, 0);
+            newState = game.Open(1, 0);
+            newState = game.Open(2, 1);
+            newState = game.Open(3, 2);
+
+            // Assert
+            PointState[,] expectedField = new PointState[4, 4] { { PointState.Opened, PointState.Opened, PointState.Closed, PointState.Closed },
+                                                                 { PointState.Opened, PointState.Opened, PointState.Closed, PointState.Closed },
+                                                                 { PointState.Closed, PointState.Opened, PointState.Closed, PointState.Closed },
+                                                                 { PointState.Closed, PointState.Closed, PointState.Opened, PointState.Closed } };
+            Assert.AreEqual(expectedField, newState.Field);
+        }
+
+        [Test]
+        public void OpenMethod_DoesNotChangeState_IfOpeningAnAlreadyOpenedCell()
+        {
+            // Arrange
+            bool[,] field = new bool[3, 3] { { false, false, false }, { false, true, false }, { false, false, false } };
+            GameProcessor game = new GameProcessor(field);
+
+            // Act
+            GameState initialState = game.Open(1, 1);
+            GameState newState = game.Open(1, 1);
+
+            // Assert
+            Assert.AreEqual(initialState, newState);
+        }
+
+        [Test]
+        public void OpenMethod_SetsStateToLost_IfOpeningAMineCell()
+        {
+            // Arrange
+            bool[,] field = new bool[3, 3] { { false, false, false }, { false, true, false }, { false, false, false } };
+            GameProcessor game = new GameProcessor(field);
+
+            // Act
+            GameState newState = game.Open(1, 1);
+
+            // Assert
+            Assert.AreEqual(GameState.Lost, newState.GameState);
+        }
+
+        [Test]
+        public void OpenMethod_SetsStateToWon_IfAllNonMineCellsAreOpened()
+        {
+            // Arrange
+            bool[,] field = new bool[3, 3] { { false, false, false }, { false, false, false }, { false, false, false } };
+            GameProcessor game = new GameProcessor(field);
+
+            // Act
+            GameState newState = game.Open(0, 0);
+            newState = game.Open(0, 1);
+            newState = game.Open(0, 2);
+            newState = game.Open(1, 0);
+            newState = game.Open(1, 1);
+            newState = game.Open(1, 2);
+            newState = game.Open(2, 0);
+            newState = game.Open(2, 1);
+            newState = game.Open(2, 2);
+
+            // Assert
+            Assert.AreEqual(GameState.Won, newState.GameState);
+        }
     }
 }
